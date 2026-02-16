d7c117595b85c68de68a5adcbf2fe269ba4573ec
kemji002 <kenji.main.mail.002@gmail.com>
2026-02-16 15:03:59 +0900

    calculator.cの変更を反映し、calculator.exeを更新しました。

--- calculator.c
+++ calculator.c
@@ -1,42 +1,36 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
-    double num1, num2;
+    
+    double num1, num2, result;
     char operator;
 
     printf("1つ目の数字を入力してください：");
     scanf("%lf", &num1);
 
+    printf("演算子を入力してください：");
+    scanf(" %c", &operator);
+
     printf("2つ目の数字を入力してください：");
     scanf("%lf", &num2);
 
-    printf("演算子を入力してください：");
-    scanf("%c", &operator);
+    switch (operator){
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/': 
+            if (num2 != 0){
+                result = num1 / num2; break;
+            }else{
+                printf("0で割ることはできません。\n"); return 1;
+            }
+        default: printf("無効な演算子です。\n"); return 1;
+    }
 
-    switch (operator)
-    {
-    case '+':
-        printf("%lf + %lf = %lf", num1, num2, num1 + num2);
-        break;
-    case '-':
-        printf("%lf - %lf = %lf", num1, num2, num1 - num2);
-        break;
-    case '*':
-        printf("%lf * %lf = %lf", num1, num2, num1 * num2);
-        break;
-    case '/':
-        if (num2 != 0)
-        {
-            printf("%lf / %lf = %lf", num1, num2, num1 / num2);
-        }
-        else
-        {
-            printf("0で割ることはできません。\n");
-        }
-        break;
-    default:
-        printf("無効な演算子です。\n");
-        break;
+    if(result==(int)result) {
+        printf("結果: %d\n", (int)result);
+    }else{
+        printf("結果: %lf\n", result);
     }
     return 0;
 }
